f383e316edaeaf3d6e735291965a7e44b760bd9b
YunChan-Oh <oyunchan9@gmail.com>
2024-06-23 04:16:02 +0900

    init

---
+++ src/main/java/muhatno/daylio/DaylioApplication.java
@@ -0,0 +1,13 @@
+package muhatno.daylio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaylioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaylioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/muhatno/daylio/DaylioApplicationTests.java
@@ -0,0 +1,13 @@
+package muhatno.daylio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaylioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
