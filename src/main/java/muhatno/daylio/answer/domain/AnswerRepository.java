38dafbe6bba581166f462b2b1a06123919879802
YunChan-Oh <oyunchan9@gmail.com>
2024-07-01 22:49:23 +0900

    add(repository) : AnswerRepository 구현

---
+++ src/main/java/muhatno/daylio/answer/domain/AnswerRepository.java
@@ -0,0 +1,6 @@
+package muhatno.daylio.answer.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnswerRepository extends JpaRepository<Answer, Integer> {
+}
