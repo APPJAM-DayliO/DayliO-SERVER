fe808f755ffe42fb6ece50d472d542a0844ac454
YunChan-Oh <oyunchan9@gmail.com>
2024-07-02 22:35:15 +0900

    add(repository) : MonthlyRepository 구현

---
+++ src/main/java/muhatno/daylio/monthly/domain/MonthlyRepository.java
@@ -0,0 +1,6 @@
+package muhatno.daylio.monthly.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MonthlyRepository extends JpaRepository<Monthly, Integer> {
+}
